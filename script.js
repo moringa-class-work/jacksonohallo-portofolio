e2493c7bace2137b5e1ec3255566a3e6165a6b23
jacksonohallo <jacksonohallo@gmail.com>
2022-07-01 17:28:31 +0300

    update qualification

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ button.addEventListener("click", function () {
   alert("Downloading");
 });
 
+document.getElementsByClassName("form").addEventListener
+  ("mouseover", function () {
+    alert("submitted")
+
+  });
+
+
+
+  let contact=document.getElementById("cont")
+
+contact.addEventListener("mouseover", function () {
+    
+  alert("Email:jacksonohallo@gmail.com")
+
+
+});
